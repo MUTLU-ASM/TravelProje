c42f4ff1456758ab266cd6f1295619d5b2bcf39c
Asim Mutlu <asimmutlu6134@gmail.com>
2023-05-08 16:18:10 +0300

    Add project files.

---
+++ TravelProje/Models/Class/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelProje.Models.Class
+{
+    public class Home
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+
+    }
+}
---
+++ TravelProje/Models/Class/Yorumlar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelProje.Models.Class
+{
+    public class Yorumlar
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Mail { get; set; }
+        public string Yorum { get; set; }
+        public int BlogId { get; set; }
+    }
+}
